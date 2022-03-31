27c0da40f058612e2e5312ced1aec2ec436410a3
Veronica-Alfr <veagalves@gmail.com>
2022-03-31 15:42:13 -0300

    Iniciando Projeto TrybeWallet

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label htmlFor="email">
+          Email:
+          <input
+            data-testid="email-input"
+            id="email"
+          />
+        </label>
+        <label htmlFor="pass">
+          Senha:
+          <input
+            data-testid="password-input"
+            id="pass"
+          />
+        </label>
+        <button type="button">Entrar</button>
+      </form>
+    );
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,2 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+// aqui é para fazer o switchCase.
